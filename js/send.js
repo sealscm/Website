5152a6168364f6b40ba500f7ae7e94a39204c532
Chris <sealscm@etsu.edu>
2022-06-16 18:11:41 -0400

    Fixed some typos and made more comments

--- js/send.js
+++ js/send.js
@@ -1,3 +1,12 @@
+/*
+    File Name:      send.js
+    Project Name:   Website Project
+    Date Created:   May-3-2022
+    Last Updated:   May-3-2022
+    Author:         Chris Seals
+    Description:    A method to send emails from the contact page.
+*/
+
 function Send(name, subject, bodyMessage)
             {
                 Email.send
